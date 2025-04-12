e1d1ab059c2e8e0e9080f0096f8d8cc0dca45308
Kidharlesh <alowoesintoluwalase@gmail.com>
2025-04-12 03:46:08 -0700

    new push

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import { AllWorks } from "../constant";
 
 const Work = () => {
-  const [all, setAll] = useState();
-  const [web, setWeb] = useState();
-  const [video, setVideo] = useState();
+  const [category, setCategory] = useState("all");
+
+  const showAll = category === "all"? AllWorks: AllWorks.filter(work => work.category === category);
+  const FirstShowAll = showAll.slice(0, 2);
+   const SecondShowAll = showAll.slice(2, 4);
 
   return (
     <section
-      className="  bg-gradient-to-r from-[#000428] to-[#020330] lg:h-screen text-[#DDDDDD] "
+      className="  bg-gradient-to-r from-[#000428] to-[#020330]  text-[#DDDDDD] pb-20 "
       id="Work"
     >
       <div className="containers  pt-10 ">
@@ -24,19 +26,62 @@ const Work = () => {
 
         <div className="flex justify-center pb-8">
           <div className=" border flex items-center gap-10 py-2 px-4 w-fit rounded-3xl bg-black border-[#200e42]">
-            <button className="">All</button>
-            <button className="">Website</button>
-            <button className="">Games</button>
-            <button className="">video</button>
+            <button
+              className={
+                category === "all" ? "bg-[#200e42] rounded-3xl p-1" : ""
+              }
+              onClick={() => setCategory("all")}
+            >
+              All
+            </button>
+            <button
+              className={
+                category === "website" ? " bg-[#200e42]  rounded-3xl p-1  " : ""
+              }
+              onClick={() => setCategory("website")}
+            >
+              Website
+            </button>
+            <button
+              className={
+                category === "game" ? " bg-[#200e42] rounded-3xl p-1 " : ""
+              }
+              onClick={() => setCategory("game")}
+            >
+              Games
+            </button>
+            <button
+              className={
+                category === "video" ? " bg-[#200e42] rounded-3xl p-1 " : ""
+              }
+              onClick={() => setCategory("video")}
+            >
+              video
+            </button>
           </div>
         </div>
-        {/* {AllWorks.map((work, index) => (
-          <div key={index}>
-            <div>
-              <img src={work.image} alt="" />
+        <div className=" flex-col  lg:flex-row flex gap-10 justify-center mt-10  ">
+          {FirstShowAll.map((work) => (
+            <div
+              key={work.id}
+              className=" border-[30px] border-b-0   lg:border-[40px]   lg:border-b-0 rounded-xl border-[#200e42]"
+            >
+              <img src={work.image} alt="" className="lg:h-96 h-72 " />
             </div>
-          </div>
-        ))} */}
+          ))}
+        </div>
+
+        <div className="flex-col  lg:flex-row flex gap-10 justify-center mt-10  ">
+          {" "}
+          {SecondShowAll.map((work) => (
+            <div
+              key={work.id}
+              className=" border-[30px] border-b-0   lg:border-[40px]   lg:border-b-0 rounded-xl border-[#200e42]"
+            >
+              <img src={work.image} alt="" className="lg:h-96 h-72 " />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/constant/index.js
+++ src/constant/index.js
@@ -1,7 +1,29 @@
-import schoolImage from "../assets/Edulah.png"
+
+import WebsiteImageTwo from "../assets/picsOne.webp";
+import GameImage from "../assets/gamePicsThree.webp";
+import WebsiteImageOne from "../assets/picsTwo.webp";
+
 
 export const AllWorks = [
   {
-    image: schoolImage,
+    id: 1,
+    image: WebsiteImageOne,
+    category: "website",
+  },
+
+  {
+    id: 2,
+    image:WebsiteImageTwo,
+    category: "video",
+  },
+  {
+    id: 3,
+    image: GameImage,
+    category: "game",
+  },
+  {
+    id: 4,
+    image: WebsiteImageTwo,
+    category: "website",
   },
 ];
