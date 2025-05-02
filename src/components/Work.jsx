30ce3b783c3f1a5b09b9e2f29f8952a8fc918321
Kidharlesh <alowoesintoluwalase@gmail.com>
2025-05-02 06:51:22 -0700

    new commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -118,7 +118,7 @@ const Work = () => {
           ))}
         </div>
         <button
-          onClick={() => alert("oh!im working on more project soon! hang on")}
+          onClick={() => alert("View more page is under construction...")}
           className="text-white border px-5 py-2 rounded-2xl bg-gradient-to-l from-[#000428] to-[#040762] text-lg border-[#2137ff] font-semibold mt-6 float-end"
         >
           View more
